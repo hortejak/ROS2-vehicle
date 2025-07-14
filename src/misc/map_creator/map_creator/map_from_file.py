a48fb36ce83d7e86f27c03f97901b1d67070b92b
hortejak <hortensky.jakub@gmail.com>
2025-07-14 20:04:59 +0200

    Map in rviz

--- src/misc/map_creator/map_creator/map_from_file.py
+++ src/misc/map_creator/map_creator/map_from_file.py
@@ -5,6 +5,7 @@ from ament_index_python.packages import get_package_share_directory
 import os
 
 from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import Pose
 
 class MapCreatorFromFile(Node):
 
@@ -17,8 +18,12 @@ class MapCreatorFromFile(Node):
         self.map_width = 0
         self.map_height = 0
 
+        frequency = 1
+
 
         self.open_txt("oval_track.txt")
+        self.map_publisher = self.create_publisher(OccupancyGrid,"/map/file",10)
+        self.create_timer(1/frequency,self.run)
 
     def open_txt(self,file_name):
 
@@ -38,7 +43,27 @@ class MapCreatorFromFile(Node):
 
         self.grid = np.array(grid)
 
-        print(self.grid.size)
+    def run(self):
+
+        o = OccupancyGrid()
+
+        o.header.stamp = self.get_clock().now().to_msg()
+        o.header.frame_id = "world"
+
+        o.info.resolution = self.resolution
+        o.info.height = self.grid.shape[0]
+        o.info.width = self.grid.shape[1]
+        origin = Pose()
+        origin.position.x = self.resolution * self.grid.shape[0] / 2
+        origin.position.y = -self.resolution * self.grid.shape[1] / 2
+
+        origin.orientation.w = 0.707
+        origin.orientation.z = 0.707
+
+        o.info.origin = origin
+        o.data = self.grid.astype(np.int8).flatten()
+
+        self.map_publisher.publish(o)
 
 
 def main(args=None):
